f444688707410d108065bb5a654b830ebfe101ae
Louis-C7 <chenlixi1@huawei.com>
2024-03-12 09:34:13 +0800

    del lib

--- lib/typescript/AnyHeader.d.ts
+++
@@ -1,6 +0,0 @@
-import { NativeProps as AnyHeaderProps } from "./fabric/AnyHeaderNativeComponent";
-import React from "react";
-export declare class AnyHeader extends React.Component<AnyHeaderProps> {
-    render(): React.JSX.Element;
-}
-export default AnyHeader;
