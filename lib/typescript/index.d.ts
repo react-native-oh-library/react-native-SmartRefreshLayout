b75481069b2ec654cc090bc879b123a6a3ead1f2
林嘉诚 <425765923@qq.com>
2024-03-16 17:46:51 +0800

    适配ohpm1.7,修复onHeaderReleasing和onHeaderPulling bug

--- lib/typescript/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { default as AnyHeader } from "./fabric/AnyHeaderNativeComponent";
-export { default as SmartRefreshControl } from "./SmartRefreshControl";
-export { default as ClassicsHeader } from "./fabric/ClassicsHeaderNativeComponent";
-export { default as StoreHouseHeader } from "./fabric/StoreHouseHeaderNativeComponent";
-export { default as DefaultHeader } from "./fabric/DefaultHeaderNativeComponent";
